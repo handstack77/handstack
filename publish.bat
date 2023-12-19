13872fa18a8e53ab22b22094894d7e61f6034304
준철 조 <junworks77@gmail.com>
2023-12-20 07:57:45 +0900

    handstack 솔루션 빌드 스크립트 추가

---
+++ publish.bat
@@ -0,0 +1,32 @@
+@echo off
+chcp 65001
+
+REM publish.bat win build Debug
+
+REM win, linux, osx
+set os_mode=%1
+if "%os_mode%" == "" set os_mode=win
+
+REM build, publish
+set action_mode=%2
+if "%action_mode%" == "" set action_mode=build
+
+REM Debug, Release
+set configuration_mode=%3
+if "%configuration_mode%" == "" set configuration_mode=Debug
+
+REM x64, x86, arm64
+set arch_mode=%4
+if "%arch_mode%" == "" set arch_mode=x64
+
+echo os_mode: %os_mode%, action_mode: %action_mode%, configuration_mode: %configuration_mode%, arch_mode: %arch_mode%
+
+rmdir /s /q C:\publish\%os_mode%-%arch_mode%\handstack
+dotnet %action_mode% 1.WebHost/ack/ack.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/app
+dotnet build 2.Modules/dbclient/dbclient.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/modules/dbclient
+dotnet build 2.Modules/function/function.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/modules/function
+dotnet build 2.Modules/logger/logger.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/modules/logger
+dotnet build 2.Modules/repository/repository.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/modules/repository
+dotnet build 2.Modules/transact/transact.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/modules/transact
+dotnet build 2.Modules/wwwroot/wwwroot.csproj --configuration %configuration_mode% --arch %arch_mode% --os %os_mode% --output C:/publish/%os_mode%-%arch_mode%/handstack/modules/wwwroot
+robocopy C:/home/handstack/contracts C:/publish/%os_mode%-%arch_mode%/handstack/contracts /s /e /copy:dat /purge
---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# publish.sh linux build Debug
+
+# win, linux, osx
+os_mode=${1:-linux}
+
+# build, publish
+action_mode=${2:-build}
+
+# Debug, Release
+configuration_mode=${3:-Debug}
+
+# x64, x86, arm64
+arch_mode=${4:-x64}
+
+echo "os_mode: $os_mode, action_mode: $action_mode, configuration_mode: $configuration_mode, arch_mode: $arch_mode"
+
+rm -rf /publish/$os_mode-$arch_mode/handstack
+dotnet $action_mode 1.WebHost/ack/ack.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/app
+dotnet build 2.Modules/dbclient/dbclient.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/modules/dbclient
+dotnet build 2.Modules/function/function.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/modules/function
+dotnet build 2.Modules/logger/logger.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/modules/logger
+dotnet build 2.Modules/repository/repository.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/modules/repository
+dotnet build 2.Modules/transact/transact.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/modules/transact
+dotnet build 2.Modules/wwwroot/wwwroot.csproj --configuration $configuration_mode --arch $arch_mode --os $os_mode --output /publish/$os_mode-$arch_mode/handstack/modules/wwwroot
+rsync -av /home/handstack/contracts /publish/$os_mode-$arch_mode/handstack/contracts
