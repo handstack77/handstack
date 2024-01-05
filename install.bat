699e7f514bdf8cbb572f2de3254b1a7f392b61c2
준철 조 <junworks77@gmail.com>
2024-01-06 08:42:40 +0900

    설치 스크립트 개선

--- install.bat
+++ install.bat
@@ -10,62 +10,69 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
-echo dotnet 및 node.js 설치 확인 중...
-
-dotnet --version | findstr /R "^8\.0\." >nul 2>nul
+where dotnet >nul 2>nul
 if %errorlevel% neq 0 (
-	winget install Microsoft.DotNet.SDK.8 -e --silent
+    echo .NET Core 8 설치를 시작합니다...
+   winget install Microsoft.DotNet.SDK.8 -e --silent
+) else (
+   dotnet --version | findstr /R "^8\.0\." >nul 2>nul
+   if %errorlevel% neq 0 (
+      echo .NET Core 8 설치를 시작합니다...
+      winget install Microsoft.DotNet.SDK.8 -e --silent
+   )
 )
 
-node --version | findstr /R "^v20\." >nul 2>nul
+where node >nul 2>nul
 if %errorlevel% neq 0 (
+    echo Node.js 20 설치를 시작합니다...
     winget install OpenJS.NodeJS.LTS -e --silent
+) else (
+   node --version | findstr /R "^v20\." >nul 2>nul
+   if %errorlevel% neq 0 (
+      echo Node.js 20 설치를 시작합니다...
+      winget install OpenJS.NodeJS.LTS -e --silent
+   )
 )
 
-echo node.js 기반 pm2, gulp, uglifyjs 설치 확인 중...
-pm2 --version | findstr /R "^5\." >nul 2>nul
+where pm2 >nul 2>nul
 if %errorlevel% neq 0 (
-	echo call install -g pm2
+    echo Node.js 기반 pm2 설치를 시작합니다...
     call npm install -g pm2
 )
 
-gulp --version | findstr /R "^CLI version" >nul 2>nul
+where gulp >nul 2>nul
 if %errorlevel% neq 0 (
-    echo install -g gulp
-    npm install -g gulp
+    echo Node.js 기반 gulp 설치를 시작합니다...
+    call npm install -g gulp
 )
 
-uglifyjs --version | findstr /R "^uglify-js" >nul 2>nul
+where uglifyjs >nul 2>nul
 if %errorlevel% neq 0 (
-    echo install -g uglify-js
+    echo Node.js 기반 uglifyjs 설치를 시작합니다...
     npm install -g uglify-js
 )
 
-echo node.js 기반 package.json 설치 확인 중...
-
 set current_path=%cd%
-
-echo current_path: %current_path%
+echo current_path: %current_path% package.json 설치 확인 중...
 
 if exist %current_path%/app/ack.dll (
-	if not exist %current_path%/node_modules (
-        echo function 모듈 %current_path%/package.json 설치
+   if not exist %current_path%/node_modules (
+        echo function 모듈 %current_path%/package.json 설치를 시작합니다...
         call npm install
         robocopy %current_path%/app/wwwroot/assets/js node_modules/syn index.js /copy:dat
-	)
+   )
 
-	if not exist %current_path%/app/node_modules (
-        echo syn.js 번들링 모듈 %current_path%/app/package.json 설치
+   if not exist %current_path%/app/node_modules (
+        echo syn.js 번들링 모듈 %current_path%/app/package.json 설치를 시작합니다...
         cd %current_path%/app
         call npm install
-	)
+   )
 
-	if not exist %current_path%/modules/wwwroot/node_modules (
-		echo syn.bundle.js 모듈 %current_path%/modules/wwwroot/package.json 설치
-		cd %current_path%/modules/wwwroot
-		call npm install
-	)
+   if not exist %current_path%/modules/wwwroot/node_modules (
+      echo syn.bundle.js 모듈 %current_path%/modules/wwwroot/package.json 설치를 시작합니다...
+      cd %current_path%/modules/wwwroot
+      call npm install
+   )
 
-	REM cd %current_path%
-	REM call dotnet %current_path%/app/ack.dll
+   echo HandStack 설치가 완료되었습니다. 터미널에서 다음 경로의 프로그램을 실행하세요. %current_path%/app/ack.exe
 )
--- install.sh
+++ install.sh
@@ -2,19 +2,31 @@
 # chmod +x /home/handstack/install.sh
 # 빌드된 프로그램 기본 디렉토리에서 ack 프로그램을 실행
 
-echo "dotnet 및 node.js 설치 확인 중..."
-if ! dotnet --version | grep -q "^8\.0\."
-then
+dotnet_path=$(which dotnet)
+if [ ! -n "$dotnet_path" ]; then
+    echo ".NET Core 8 설치를 시작합니다..."
     wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update
     sudo apt-get install -y apt-transport-https
     sudo apt-get update
     sudo apt-get install -y dotnet-sdk-8.0
+else
+    dotnet_version=$(dotnet --version | grep -E "^8\.")
+    if [ ! -n "$dotnet_version" ]; then
+        echo ".NET Core 8 설치를 시작합니다..."
+        wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+        sudo dpkg -i packages-microsoft-prod.deb
+        sudo apt-get update
+        sudo apt-get install -y apt-transport-https
+        sudo apt-get update
+        sudo apt-get install -y dotnet-sdk-8.0
+    fi
 fi
 
-if ! node --version | grep -q "^v20\."
-then
+node_path=$(which node)
+if [ ! -n "$node_path" ]; then
+    echo "Node.js 20 설치를 시작합니다..."
     sudo apt-get update
     sudo apt-get install -y ca-certificates curl gnupg
     sudo mkdir -p /etc/apt/keyrings
@@ -22,50 +34,59 @@ then
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
     sudo apt-get update
     sudo apt-get install -y nodejs
+else
+    node_version=$(node --version | grep -E "^v20\.")
+    if [ ! -n "$node_version" ]; then
+        echo "Node.js 20 설치를 시작합니다..."
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl gnupg
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+        sudo apt-get update
+        sudo apt-get install -y nodejs
+    fi
 fi
 
-echo "node.js 기반 pm2, gulp, uglifyjs 설치 확인 중..."
-if ! pm2 --version | grep -q "^5\."
-then
-    echo "call install -g pm2"
+pm2_path=$(which pm2)
+if [ ! -n "pm2_path" ]; then
+    echo "Node.js 기반 pm2 설치를 시작합니다..."
     npm install -g pm2
 fi
 
-if ! gulp --version | grep -q "^CLI version"
-then
-    echo "install -g gulp"
+gulp_path=$(which gulp)
+if [ ! -n "gulp_path" ]; then
+    echo "Node.js 기반 gulp 설치를 시작합니다..."
     npm install -g gulp
 fi
 
-if ! uglifyjs --version | grep -q "^uglify-js"
-then
-    echo "install -g uglify-js"
+uglifyjs_path=$(which uglifyjs)
+if [ ! -n "uglifyjs_path" ]; then
+    echo "Node.js 기반 uglifyjs 설치를 시작합니다..."
     npm install -g uglify-js
 fi
 
-echo "node.js 기반 package.json 설치 확인 중..."
 current_path=$(pwd)
-echo "current_path: $current_path"
+echo "current_path: $current_path node.js 기반 package.json 설치 확인 중..."
 
 if [ -f "$current_path/app/ack.dll" ]; then
     if [ ! -d "$current_path/node_modules" ]; then
-        echo "function 모듈 $current_path/package.json 설치"
+        echo "function 모듈 $current_path/package.json 설치를 시작합니다..."
         npm install
         rsync -av --progress --exclude='*' --include='index.js' %current_path%/app/wwwroot/assets/js/ node_modules/syn/
     fi
 
     if [ ! -d "$current_path/app/node_modules" ]; then
-        echo "syn.js 번들링 모듈 $current_path/app/package.json 설치"
+        echo "syn.js 번들링 모듈 $current_path/app/package.json 설치를 시작합니다..."
         cd $current_path/app
         npm install
     fi
 
     if [ ! -d "$current_path/modules/wwwroot/node_modules" ]; then
-        echo "syn.bundle.js 모듈 $current_path/modules/wwwroot/package.json 설치"
+        echo "syn.bundle.js 모듈 $current_path/modules/wwwroot/package.json 설치를 시작합니다..."
         cd $current_path/modules/wwwroot
         npm install
     fi
 
-    # cd $current_path
-    # dotnet $current_path/app/ack.dll
+    echo "HandStack 설치가 완료되었습니다. 터미널에서 다음 경로의 프로그램을 실행하세요. $current_path/app/ack"
 fi
