5e7f0a7ee2da84ad0a25dcc3fd537e0c8446db91
준철 조 <junworks77@gmail.com>
2024-01-11 09:01:17 +0900

    공통 wwwroot 디렉토리 및 파일을 lib 제외한 복사하는 예제 batch 스크립트 추가

--- 2.Modules/wwwroot/build.bat
+++ 2.Modules/wwwroot/build.bat
@@ -1,7 +1,10 @@
 @echo off
+
+:: robocopy wwwroot ../../1.WebHost/build/handstack/modules/wwwroot/wwwroot /e /copy:dat /xd C:\projects\handstack77\handstack\2.Modules\wwwroot\wwwroot\lib
+
 :: call pm2 stop ack
 :: :: dotnet msbuild C:/projects/handstack/2.Modules/wwwroot/wwwroot.csproj
-robocopy bin/Debug/net8.0 C:/home/handstack/modules/wwwroot /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
+:: robocopy bin/Debug/net8.0 C:/home/handstack/modules/wwwroot /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
 :: pm2 restart ack
 
 :: robocopy wwwroot C:/home/handstack/modules/wwwroot/wwwroot /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
