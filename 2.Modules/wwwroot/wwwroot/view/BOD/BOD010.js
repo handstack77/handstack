29fd297c43d77868f13b688a9ef792977bc7db37
handstack77 <handstack77@gmail.com>
2024-01-27 15:19:26 +0900

    예제 코드 리팩토링

--- 2.Modules/wwwroot/wwwroot/view/BOD/BOD010.js
+++ 2.Modules/wwwroot/wwwroot/view/BOD/BOD010.js
@@ -37,10 +37,35 @@ let $BOD010 = {
         },
 
         btnNewBoard_click(evt) {
-            var windowID = 'BOD010';
-            var popupOptions = $object.clone(syn.$w.popupOptions);
-            popupOptions.title = '신규 게시글 작성';
-            popupOptions.src = 'BOD011.html';
+            $this.method.openPopup('게시글 확인 및 편집', 'BOD012.html');
+        },
+
+        btnSearch_click(evt) {
+            syn.$w.transactionAction('LD01');
+        },
+
+        grdBoard_cellButtonClick(elID, row, column, prop, value) {
+            var gridID = 'grdBoard';
+            var columnViewPost = syn.uicontrols.$grid.propToCol(gridID, 'EditPost');
+            if (column == columnViewPost && row > -1) {
+                $this.method.openPopup('게시글 확인 및 편집', 'BOD012.html', (elID) => {
+                    let post = {
+                        postID: syn.uicontrols.$grid.getDataAtCell(gridID, row, 'ID'),
+                        title: syn.uicontrols.$grid.getDataAtCell(gridID, row, 'Title')
+                    };
+
+                    syn.$n.call('BOD010', 'request', post);
+                });
+            };
+        }
+    },
+
+    method: {
+        openPopup(title, src, callback) {
+            let windowID = 'BOD010';
+            let popupOptions = $object.clone(syn.$w.popupOptions);
+            popupOptions.title = title;
+            popupOptions.src = src;
             popupOptions.channelID = windowID;
             popupOptions.isModal = true;
             popupOptions.width = 800;
@@ -53,42 +78,7 @@ let $BOD010 = {
                 }
             });
 
-            syn.$w.windowOpen(windowID, popupOptions);
-        },
-
-        btnSearch_click(evt) {
-            syn.$w.transactionAction('LD01');
-        },
-
-        grdBoard_cellButtonClick(elID, row, column, prop, value) {
-            var gridID = 'grdBoard';
-            var columnViewPost = syn.uicontrols.$grid.propToCol(gridID, 'EditPost');
-            if (column == columnViewPost && row > -1) {
-                var windowID = 'BOD010';
-                var popupOptions = $object.clone(syn.$w.popupOptions);
-                popupOptions.title = '게시글 확인 및 편집';
-                popupOptions.src = 'BOD012.html';
-                popupOptions.channelID = windowID;
-                popupOptions.isModal = true;
-                popupOptions.width = 800;
-                popupOptions.height = 750;
-                popupOptions.notifyActions.push({
-                    actionID: 'response',
-                    handler(evt, val) {
-                        syn.$w.windowClose(windowID);
-                        syn.$w.transactionAction('LD01');
-                    }
-                });
-
-                syn.$w.windowOpen(windowID, popupOptions, (elID) => {
-                    var post = {
-                        postID: syn.uicontrols.$grid.getDataAtCell(gridID, row, 'ID'),
-                        title: syn.uicontrols.$grid.getDataAtCell(gridID, row, 'Title')
-                    };
-
-                    syn.$n.call(windowID, 'request', post);
-                });
-            };
+            syn.$w.windowOpen(windowID, popupOptions, callback);
         }
     }
 }
