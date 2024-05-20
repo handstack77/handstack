19736215efc8a762c4eb5bc9be40c21834821fe1
준철 조 <handstack77@gmail.com>
2024-05-20 10:10:36 +0900

    거래 응답 결과를 DataSet 으로 변환 기능 추가

--- 3.Infrastructure/HandStack.Web/Extensions/TransactionClientExtensions.cs
+++ 3.Infrastructure/HandStack.Web/Extensions/TransactionClientExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 
 using HandStack.Web.Entity;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HandStack.Web.Extensions
 {
@@ -9,6 +13,37 @@ namespace HandStack.Web.Extensions
         public static void Add(this IList<ServiceParameter> parameters, string parameterName, object? value)
         {
             parameters.Add(new ServiceParameter() { prop = parameterName, val = value });
+        }
+
+        public static DataSet? ToDataSet(this Dictionary<string, JToken> transactionResult)
+        {
+            DataSet? result = null;
+            if (transactionResult != null && transactionResult.Count > 0)
+            {
+                result = new DataSet();
+                foreach (var dataItem in transactionResult)
+                {
+                    DataTable? dataTable = null;
+                    var jToken = dataItem.Value;
+
+                    if (jToken is JObject)
+                    {
+                        dataTable = JsonConvert.DeserializeObject<DataTable>($"[{jToken}]");
+                    }
+                    else if (jToken is JArray)
+                    {
+                        dataTable = JsonConvert.DeserializeObject<DataTable>($"{jToken}");
+                    }
+
+                    if (dataTable != null)
+                    {
+                        dataTable.TableName = dataItem.Key;
+                        result.Tables.Add(dataTable);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
