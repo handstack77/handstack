8c1e473b42e027ea94f265bf7dc2e6a828816011
handstack77 <handstack77@gmail.com>
2024-06-17 23:06:57 +0900

    Node.js 기반 checkip 서버 추가

---
+++ 1.WebHost/ack/checkip.js
@@ -0,0 +1,17 @@
+﻿// npm install express cors request-ip
+
+const express = require('express');
+const requestIp = require('request-ip');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(requestIp.mw())
+
+app.get('/checkip', (req, res) => {
+    var ip = req.clientIp;
+    res.send(ip);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`checkip Server running on port ${PORT}`));
