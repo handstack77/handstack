bbcb1158980e3b70a01983d09c4ff15068b36ea8
handstack77 <handstack77@gmail.com>
2024-06-17 23:16:14 +0900

    ip4 를 반환하도록 개선

--- 1.WebHost/ack/checkip.js
+++ 1.WebHost/ack/checkip.js
@@ -10,6 +10,9 @@ app.use(requestIp.mw())
 
 app.get('/checkip', (req, res) => {
     var ip = req.clientIp;
+    if (ip.substring(0, 7) == "::ffff:") {
+        ip = ip.substring(7);
+    }
     res.send(ip);
 });
 
