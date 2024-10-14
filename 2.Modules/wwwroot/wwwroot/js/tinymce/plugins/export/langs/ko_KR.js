d7eb081878201b71ab51d7f56568c2c3b4eb5a65
junworks77 <junworks77@gmail.com>
2024-10-14 12:28:44 +0900

    코드 정리

--- 2.Modules/wwwroot/wwwroot/js/tinymce/plugins/export/langs/ko_KR.js
+++
@@ -1,6 +0,0 @@
-tinymce.addI18n("ko_KR", {
-  "Export": "\ub0b4\ubcf4\ub0b4\uae30",
-  "PDF": "PDF",
-  'Failed to load the "{0}" exporter': '"{0}" exporter \ubd88\ub7ec\uc624\uae30 \uc2e4\ud328',
-  "Failed to export to PDF due to browser limitations": "\ube0c\ub77c\uc6b0\uc800 \ud55c\uacc4\ub85c \uc778\ud574 PDF\ub85c \ub0b4\ubcf4\ub0b4\uae30 \uc2e4\ud328"
-});
