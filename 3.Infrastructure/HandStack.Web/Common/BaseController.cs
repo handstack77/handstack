31cddebf61a5830fdc55890f595b61b0e08e0b6a
junworks77 <junworks77@gmail.com>
2024-12-18 15:02:33 +0900

    API Controller 에서 모듈 사용자 검증 기능 추가

--- 3.Infrastructure/HandStack.Web/Common/BaseController.cs
+++ 3.Infrastructure/HandStack.Web/Common/BaseController.cs
@@ -1,9 +1,18 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
+using HandStack.Core.ExtensionMethod;
 using HandStack.Web.Entity;
 
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using Serilog;
 
 namespace HandStack.Web.Common
 {
@@ -15,6 +24,65 @@ namespace HandStack.Web.Common
         public BaseController()
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("ko-KR");
+        }
+
+        protected async Task<bool> VerifyUserRole(HttpContext? httpContext, string scheme, string roleID)
+        {
+            bool result = false;
+            if (httpContext != null)
+            {
+                try
+                {
+                    var authenticateResult = await httpContext.AuthenticateAsync(scheme);
+                    if (authenticateResult.Succeeded == true)
+                    {
+                        var principal = authenticateResult.Principal;
+                        if (principal?.Identity?.IsAuthenticated == true)
+                        {
+                            var roles = principal.FindFirst("Roles")?.Value;
+                            if (roles != null)
+                            {
+                                if (roles.SplitComma().IndexOf(roleID) > -1)
+                                {
+                                    result = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Log.Logger.Error(exception, "[{LogCategory}] " + $"scheme: {scheme}, roleID: {roleID} 검증 오류", "BaseController/VerifyUserRole");
+                }
+            }
+
+            return result;
+        }
+
+        protected async Task<List<Claim>> GetPrincipalClaims(HttpContext? httpContext, string scheme)
+        {
+            List<Claim> result = new List<Claim>();
+            if (httpContext != null)
+            {
+                try
+                {
+                    var authenticateResult = await httpContext.AuthenticateAsync(scheme);
+                    if (authenticateResult.Succeeded == true)
+                    {
+                        var principal = authenticateResult.Principal;
+                        if (principal?.Identity?.IsAuthenticated == true)
+                        {
+                            result = principal.Claims.ToList();
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Log.Logger.Error(exception, "[{LogCategory}] " + $"scheme: {scheme} 확인 오류", "BaseController/GetPrincipalClaims");
+                }
+            }
+
+            return result;
         }
     }
 }
