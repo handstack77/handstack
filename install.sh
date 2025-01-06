afb7ab6ca1581801c3e30fbc8dc9c055657313bf
junworks77 <junworks77@gmail.com>
2025-01-06 14:04:02 +0900

    기존 환경변수값 초기화후 추가 하도록 개선

--- install.sh
+++ install.sh
@@ -50,11 +50,14 @@ fi
 
 current_path=$(pwd)
 if [ -f "$current_path/1.WebHost/ack/ack.csproj" ]; then
+    sudo sed -i '/export HANDSTACK_SRC=/d' /etc/profile
     echo "export HANDSTACK_SRC=\"$current_path\"" | sudo tee -a /etc/profile
     export HANDSTACK_SRC="$current_path"
     HANDSTACK_SRC="$current_path"
 
     mkdir -p $current_path/1.WebHost/build/handstack
+    
+    sudo sed -i '/export HANDSTACK_HOME=/d' /etc/profile
     echo "export HANDSTACK_HOME=\"$current_path/1.WebHost/build/handstack\"" | sudo tee -a /etc/profile
     export HANDSTACK_HOME="$current_path/1.WebHost/build/handstack"
     HANDSTACK_HOME="$current_path/1.WebHost/build/handstack"
