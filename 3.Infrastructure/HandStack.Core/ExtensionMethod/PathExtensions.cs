42b2bd0e6dd65f041cd7f0728c9867d18299f8bf
junworks77 <junworks77@gmail.com>
2025-01-16 09:53:29 +0900

    Path.Combine, Path.Join 기본 구분자를 '/' 으로 사용하는 확장 메서드 추가

---
+++ 3.Infrastructure/HandStack.Core/ExtensionMethod/PathExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+
+namespace HandStack.Core.ExtensionMethod
+{
+    public static class PathExtensions
+    {
+        public static string Combine(string path1, string path2, char separator = '/')
+        {
+            string combinedPath = Path.Combine(path1, path2);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Combine(string path1, string path2, string path3, char separator = '/')
+        {
+            string combinedPath = Path.Combine(path1, path2, path3);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Combine(string path1, string path2, string path3, string path4, char separator = '/')
+        {
+            string combinedPath = Path.Combine(path1, path2, path3, path4);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Combine(string path1, string path2, string path3, string path4, string path5, char separator = '/')
+        {
+            string combinedPath = Path.Combine(path1, path2, path3, path4, path5);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Combine(string[] paths, char separator = '/')
+        {
+            string combinedPath = Path.Combine(paths);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Join(string path1, string path2, char separator = '/')
+        {
+            string combinedPath = Path.Join(path1, path2);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Join(string path1, string path2, string path3, char separator = '/')
+        {
+            string combinedPath = Path.Join(path1, path2, path3);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Join(string path1, string path2, string path3, string path4, char separator = '/')
+        {
+            string combinedPath = Path.Join(path1, path2, path3, path4);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Join(string path1, string path2, string path3, string path4, string path5, char separator = '/')
+        {
+            string combinedPath = Path.Join(path1, path2, path3, path4, path5);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+
+        public static string Join(string[] paths, char separator = '/')
+        {
+            string combinedPath = Path.Join(paths);
+            return combinedPath.Replace('\\', separator).Replace('/', separator);
+        }
+    }
+}
