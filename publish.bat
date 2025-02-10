9b2d4dd695c2d96d6a772621fe77825ab0efaee9
junworks77 <junworks77@gmail.com>
2025-02-10 10:26:38 +0900

    게시 zip 파일 경로 출력

--- publish.bat
+++ publish.bat
@@ -63,4 +63,6 @@ del /F /Q "%wwwroot_js_path%\js\syn.scripts.js"
 del /F /Q "%wwwroot_js_path%\js\syn.scripts.min.js"
 
 tar -czf ../publish/%os_mode%-%arch_mode%.zip -C ../publish %os_mode%-%arch_mode%
+set zip_path=%cd%\publish\%os_mode%-%arch_mode%.zip
+echo %zip_path%
 REM git archive --format zip --output ../publish/handstack-src.zip master
