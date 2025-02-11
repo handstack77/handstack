8fab43356160f372b7ae9c09831fdea1a2ffc9a4
junworks77 <junworks77@gmail.com>
2025-02-12 08:21:04 +0900

    ack 프로그램 실행 옵트아웃 옵션 추가

--- 4.Tool/CLI/edgeproxy/Program.cs
+++ 4.Tool/CLI/edgeproxy/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +12,8 @@ namespace edgeproxy
 {
     public class Program
     {
+        private static Process? ackProcess;
+
         public static void Main(string[] args)
         {
             var isDaemonService = (args.Contains("--d") == true || args.Contains("--daemon") == true);
@@ -21,7 +26,82 @@ namespace edgeproxy
             }
 
             var host = builder.Build();
+
+            var handstackHome = Environment.GetEnvironmentVariable("HANDSTACK_HOME");
+            if (string.IsNullOrEmpty(handstackHome))
+            {
+                Console.WriteLine("HANDSTACK_HOME 환경변수가 설정되지 않았습니다.");
+                Environment.Exit(1);
+            }
+
+            var processes = Process.GetProcessesByName("ack");
+            var entryAckDllFileName = Path.Join(handstackHome, "app", "ack.dll");
+            if (File.Exists(entryAckDllFileName) == true && processes.Any() == false)
+            {
+                bool isAckProcessRun = true;
+                var ackProcessRun = args.FirstOrDefault(arg => arg.StartsWith("--ackrun="))?.Split('=')[1];
+                if (bool.TryParse(ackProcessRun, out isAckProcessRun) == false)
+                {
+                    isAckProcessRun = true;
+                }
+
+                if (isAckProcessRun == true)
+                {
+                    var entryCommand = Path.Join(handstackHome, "app", "ack");
+                    var entryArguments = args.FirstOrDefault(arg => arg.StartsWith("--arguments="))?.Replace("--arguments=", "");
+                    if (string.IsNullOrEmpty(entryArguments) == false)
+                    {
+                        entryCommand = $"{entryCommand} {entryArguments}";
+                    }
+
+                    ExecuteEntryCommand(entryCommand);
+                }
+            }
+            else
+            {
+                ackProcess = processes.FirstOrDefault();
+            }
+
+            bool isAckProcessExit = true;
+            var ackProcessExit = args.FirstOrDefault(arg => arg.StartsWith("--ackexit="))?.Split('=')[1];
+            if (bool.TryParse(ackProcessExit, out isAckProcessExit) == false)
+            {
+                isAckProcessExit = true;
+            }
+
+            if (isAckProcessExit == true)
+            {
+                AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+                {
+                    if (ackProcess != null && ackProcess.HasExited == false)
+                    {
+                        ackProcess.Kill(true);
+                        ackProcess.WaitForExit();
+                    }
+                };
+            }
+
             host.Run();
+        }
+
+        private static void ExecuteEntryCommand(string command)
+        {
+            var processInfo = new ProcessStartInfo();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                processInfo.FileName = "cmd.exe";
+                processInfo.Arguments = $"/c {command}";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                processInfo.FileName = "/bin/bash";
+                processInfo.Arguments = $"-c \"{command}\"";
+            }
+
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            ackProcess = Process.Start(processInfo);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
