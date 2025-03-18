1392f2424f0c0dfc5612c4360e947e62e8e300cf
junworks77 <junworks77@gmail.com>
2025-03-19 08:28:27 +0900

    null 문자열 허용

--- 3.Infrastructure/HandStack.Web/GlobalConfiguration.cs
+++ 3.Infrastructure/HandStack.Web/GlobalConfiguration.cs
@@ -115,7 +115,7 @@ namespace HandStack.Web
 
         public static string DefaultCulture => "ko-KR";
 
-        public static string GetBasePath(string basePath, string? defaultPath = "")
+        public static string GetBasePath(string? basePath, string? defaultPath = "")
         {
             basePath = Environment.ExpandEnvironmentVariables(basePath.ToStringSafe());
             basePath = string.IsNullOrEmpty(basePath) == true ? "" : (basePath.StartsWith(".") == true ? Path.GetFullPath(basePath, EntryBasePath) : new DirectoryInfo(basePath).FullName.Replace("\\", "/"));
