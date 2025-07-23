6cf74fc32e45e37a2372129983ea4492a8595db2
junworks77 <junworks77@gmail.com>
2025-07-23 12:57:45 +0900

    미사용중인 기본 모듈 정리

--- 2.Modules/openapi/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: call pm2 stop ack
-:: dotnet msbuild C:/projects/handstack/2.Modules/openapi/openapi.csproj
-robocopy bin/Debug/net8.0 C:/home/handstack/modules/openapi /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
-:: pm2 restart ack
-
-:: robocopy openapi C:/home/handstack/modules/openapi/wwwroot /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
--- 2.Modules/prompter/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: call pm2 stop ack
-:: dotnet msbuild C:/projects/handstack/2.Modules/prompter/prompter.csproj
-robocopy bin/Debug/net8.0 C:/home/handstack/modules/prompter /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
-:: pm2 restart ack
-
-:: robocopy prompter C:/home/handstack/modules/prompter/wwwroot /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
