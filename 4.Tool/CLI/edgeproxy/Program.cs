9f6d9b46b99930227666908def579452f8ab41e8
junworks77 <junworks77@gmail.com>
2026-03-10 19:14:43 +0900

    빌드 구성 변경

--- 4.Tool/CLI/edgeproxy/Program.cs
+++ 4.Tool/CLI/edgeproxy/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 using Microsoft.AspNetCore.Builder;
