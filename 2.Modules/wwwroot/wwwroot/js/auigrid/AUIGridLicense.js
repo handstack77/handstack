e90cc34f4798d6c5e4e9427585f819aa3ce317e8
junworks77 <junworks77@gmail.com>
2026-03-17 15:36:10 +0900

    AUISoft 데모 키 변경

--- 2.Modules/wwwroot/wwwroot/js/auigrid/AUIGridLicense.js
+++ 2.Modules/wwwroot/wwwroot/js/auigrid/AUIGridLicense.js
@@ -1,31 +1,6 @@
-/*!
- * AUIGrid Non-Commercial LICENSE v3.0.16
- * Authorized Domain(or IP) : localhost, 127.0.0.1
- * www.auisoft.net
- *
- * 1. License Grant
- * Under the terms of this agreement, AUISoft Co., Ltd. (hereinafter "Licensor") grants you a perpetual, worldwide, non-exclusive, royalty-free, and non-transferable license to use AUI Products (hereinafter "Software") for non-commercial purposes only.
- * 2. Permitted Use
- * You may use the software for the following non-commercial purposes:
- * Educational, academic, and research purposes
- * Personal use
- * Testing, development, and demonstration purposes
- * Use exclusively in a localhost environment
- * 3. Prohibited Use
- * You may not use the software for the following commercial purposes:
- * Business or professional operations
- * Licensing, renting, exchanging, selling, or distributing the software or its modifications
- * Including the software in commercial products or services
- * Use by government agencies or international organizations
- * 4. Non-Commercial Evaluation Use
- * Employees of a company may use the software for evaluation, development, and testing purposes only in non-commercial environments.
- * 5. Commercial Use Notice
- * To use the software for commercial purposes, you must obtain an appropriate license from the Licensor.For commercial use inquiries, please contact the Company.
- * 6. Localhost Usage Restriction
- * This software is permitted to be used only in a localhost environment.If the software is uploaded to an external web server where a separate access domain or access IP exists, a valid commercial license must be purchased.
- *
- * Copyright © AUISoft Co., Ltd. https://www.auisoft.net
+﻿/**
+ * AUIGrid 3.0.16 License
+ * Client ID : AUISoft DEMO
  */
-/* eslint-disable */
-const AUIGridLicense = 'eyJjdCI6Ikc5QWExc0tScDNBYytmZGhFS1VLNXVHYjdhTk81dmZiVmFHRG5VZWVVT3YwOXlkSFRtZ2VjakpLeU1MMWxpdmNaL2MxdXh4eEc5cU9UR1ZyUVl5NTdBRmNVTG00NDRyYW1DR0xWT3pPV0gwPSIsIml2IjoiMmEwZjZjN2Q5YzUyZGVhMWFiZjliMGY0NGIwYTkyNWUiLCJzIjoiYTlkOTJhOWI3NWZhMTBlYiJ9';
-if (typeof window !== 'undefined') window.AUIGridLicense = AUIGridLicense;
+const AUIGridLicense = "eyJjdCI6ImdvYm9DbzdKMkZjQXJxK0x4TWlVb1RMY09rT0UzMFE3eEJRcml0cFcxajRWUDhsQUxoazgzbHBSeDR2aEJuNFlVeVpkYmUxY29JK0RGTmVkTENDME50cEZZcmkvN2NRdjZhN2NicXZuejh4YU82VXdxS2VFK0srSGx4c1puTEthIiwiaXYiOiJlMDUxYWY0OTlmMzIzYjVhZmE1N2YwNmYzOWFiOTIwYSIsInMiOiI0Zjc2NDRhNDEwZTQyMmNlIn0=";
+if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
