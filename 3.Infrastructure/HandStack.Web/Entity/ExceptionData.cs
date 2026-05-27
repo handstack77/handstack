a3a61f636ec9e592b67faf62e411179528a3b1f0
junworks77 <junworks77@gmail.com>
2026-05-27 14:30:27 +0900

    예외 정보 처리를 위한 Entity 추가

---
+++ 3.Infrastructure/HandStack.Web/Entity/ExceptionData.cs
@@ -0,0 +1,15 @@
+﻿namespace HandStack.Web.Entity
+{
+    public partial record ExceptionData
+    {
+        public string Error { get; set; } = string.Empty;
+
+        public string Level { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public string StackTrace { get; set; } = string.Empty;
+
+        public string TypeMember { get; set; } = string.Empty;
+    }
+}
