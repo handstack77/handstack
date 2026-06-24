2a962baeeb114f3024d5dda2ccc0ecd2797ecbb9
junworks77 <junworks77@gmail.com>
2026-06-24 13:31:55 +0900

    YamlDotNet 지원 삭제

--- 3.Infrastructure/HandStack.Web/Helpers/JsonHelper.cs
+++ 3.Infrastructure/HandStack.Web/Helpers/JsonHelper.cs
@@ -8,9 +8,6 @@ using HandStack.Web.Enumeration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-using YamlDotNet.Core;
-using YamlDotNet.Serialization;
-
 namespace HandStack.Web.Helper
 {
     public static class JsonHelper
@@ -124,70 +121,6 @@ namespace HandStack.Web.Helper
             catch (Exception exception)
             {
                 return exception.Message;
-            }
-        }
-
-        public static string? ConvertFromYaml(string? input, Indentation indentationMode)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return string.Empty;
-            }
-
-            try
-            {
-                using var stringReader = new StringReader(input);
-
-                var deserializer = new DeserializerBuilder()
-                    .WithNodeTypeResolver(new DecimalYamlTypeResolver())
-                    .WithNodeTypeResolver(new BooleanYamlTypeResolver())
-                    .Build();
-
-                var yamlObject = deserializer.Deserialize(stringReader);
-
-                if (yamlObject is null or string)
-                {
-                    return null;
-                }
-
-                var stringBuilder = new StringBuilder();
-                using (var stringWriter = new StringWriter(stringBuilder))
-                using (var jsonTextWriter = new JsonTextWriter(stringWriter))
-                {
-                    switch (indentationMode)
-                    {
-                        case Indentation.TwoSpaces:
-                            jsonTextWriter.Formatting = Formatting.Indented;
-                            jsonTextWriter.IndentChar = ' ';
-                            jsonTextWriter.Indentation = 2;
-                            break;
-
-                        case Indentation.FourSpaces:
-                            jsonTextWriter.Formatting = Formatting.Indented;
-                            jsonTextWriter.IndentChar = ' ';
-                            jsonTextWriter.Indentation = 4;
-                            break;
-
-                        default:
-                            throw new NotSupportedException();
-                    }
-
-                    var jsonSerializer = JsonSerializer.CreateDefault(new JsonSerializerSettings()
-                    {
-                        Converters = { new DecimalJsonConverter() }
-                    });
-                    jsonSerializer.Serialize(jsonTextWriter, yamlObject);
-                }
-
-                return stringBuilder.ToString();
-            }
-            catch (SemanticErrorException exception)
-            {
-                return exception.Message;
-            }
-            catch
-            {
-                return string.Empty;
             }
         }
 
