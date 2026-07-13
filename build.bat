3f376f25f1cbedd56600ec3e46bad1a1967d7e7d
junworks77 <junworks77@gmail.com>
2026-07-13 17:15:16 +0900

    키워드 문법 오류 수정

--- build.bat
+++ build.bat
@@ -5,29 +5,7 @@ setlocal EnableExtensions EnableDelayedExpansion
 
 set "CONFIG=Debug"
 
-set PROJECTS=^
-"2.Modules\wwwroot\wwwroot.csproj" ^
-"2.Modules\dbclient\dbclient.csproj" ^
-"2.Modules\graphclient\graphclient.csproj" ^
-"2.Modules\function\function.csproj" ^
-"2.Modules\command\command.csproj" ^
-"2.Modules\prompter\prompter.csproj" ^
-"2.Modules\logger\logger.csproj" ^
-"2.Modules\repository\repository.csproj" ^
-"2.Modules\transact\transact.csproj" ^
-"2.Modules\checkup\checkup.csproj" ^
-"1.WebHost\ack\ack.csproj" ^
-"1.WebHost\agent\agent.csproj" ^
-"1.WebHost\deploy\deploy.csproj" ^
-"1.WebHost\forbes\forbes.csproj" ^
-"4.Tool\CLI\bundling\bundling.csproj" ^
-"4.Tool\CLI\dotnet-installer\dotnet-installer.csproj" ^
-"4.Tool\CLI\edgeproxy\edgeproxy.csproj" ^
-"4.Tool\CLI\dbplatform\dbplatform.csproj" ^
-"4.Tool\CLI\handsonapp\handsonapp.csproj" ^
-"4.Tool\CLI\handstack\handstack.csproj" ^
-"4.Tool\CLI\ports\ports.csproj" ^
-"4.Tool\CLI\updater\updater.csproj"
+set PROJECTS="2.Modules\wwwroot\wwwroot.csproj" "2.Modules\dbclient\dbclient.csproj" "2.Modules\graphclient\graphclient.csproj" "2.Modules\function\function.csproj" "2.Modules\command\command.csproj" "2.Modules\prompter\prompter.csproj" "2.Modules\logger\logger.csproj" "2.Modules\repository\repository.csproj" "2.Modules\transact\transact.csproj" "2.Modules\checkup\checkup.csproj" "1.WebHost\ack\ack.csproj" "1.WebHost\agent\agent.csproj" "1.WebHost\deploy\deploy.csproj" "1.WebHost\forbes\forbes.csproj" "4.Tool\CLI\bundling\bundling.csproj" "4.Tool\CLI\dotnet-installer\dotnet-installer.csproj" "4.Tool\CLI\edgeproxy\edgeproxy.csproj" "4.Tool\CLI\dbplatform\dbplatform.csproj" "4.Tool\CLI\handsonapp\handsonapp.csproj" "4.Tool\CLI\handstack\handstack.csproj" "4.Tool\CLI\ports\ports.csproj" "4.Tool\CLI\updater\updater.csproj"
 
 echo Restoring solution packages...
 dotnet restore handstack.sln
